d600bd33d58175de901151325e5c30dc1fb7f367
willyhorizont <willyhorizont@gmail.com>
2025-04-28 23:13:06 +0700

    small update in kalender-libur-generator 20250428-1-0-2

--- utils.js
+++ utils.js
@@ -17,3 +17,56 @@ export const isFunction = (anything) => (Object.prototype.toString.call(anything
 export const getType = (anything) => ((isNull(anything) === true) ? jsType.Null : ((isBoolean(anything) === true) ? jsType.Boolean : ((isString(anything) === true) ? jsType.String : ((isNumeric(anything) === true) ? jsType.Numeric : ((isObject(anything) === true) ? jsType.Object : ((isArray(anything) === true) ? jsType.Array : ((isFunction(anything) === true) ? jsType.Function : Object.prototype.toString.call(anything))))))));
 
 export const jsonStringify = (anything, { pretty = false, indent = " ".repeat(4), indentLevel = 0, argumentType = getType(anything) } = {}) => ((argumentType === jsType.Null) ? "null" : ((argumentType === jsType.String) ? `"${anything}"` : (((argumentType === jsType.Numeric) || (argumentType === jsType.Boolean)) ? `${anything}` : ((argumentType === jsType.Object) ? ((Object.keys(anything).length === 0) ? "{}" : (`${((pretty === true) ? (`{\n${indent.repeat(indentLevel + 1)}`) : "{ ")}${Object.entries(anything).reduce((currentResult, [objectKey, objectValue], objectEntryIndex) => (`${currentResult}${(((objectEntryIndex + 1) !== Object.keys(anything).length) ? `"${objectKey}": ${jsonStringify(objectValue, { pretty, indentLevel: (indentLevel + 1) })}${((pretty === true) ? (`,\n${indent.repeat(indentLevel + 1)}`) : ", ")}` : `"${objectKey}": ${jsonStringify(objectValue, { pretty, indentLevel: (indentLevel + 1) })}`)}`), "")}${((pretty === true) ? (`\n${indent.repeat(indentLevel)}}`) : " }")}`)) : ((argumentType === jsType.Array) ? ((anything.length === 0) ? "[]" : (`${((pretty === true) ? (`[\n${indent.repeat(indentLevel + 1)}`) : "[")}${anything.reduce((currentResult, arrayItem, arrayItemIndex) => ((((arrayItemIndex + 1) !== anything.length) ? `${currentResult}${jsonStringify(arrayItem, { pretty, indentLevel: (indentLevel + 1) })}${((pretty === true) ? (`,\n${indent.repeat(indentLevel + 1)}`) : ", ")}` : `${currentResult}${jsonStringify(arrayItem, { pretty, indentLevel: (indentLevel + 1) })}`)), "")}${((pretty === true) ? (`\n${indent.repeat(indentLevel)}]`) : "]")}`)) : ((argumentType === jsType.Function) ? "[object Function]" : argumentType)))))); // custom JSON.stringify() function jsonStringifyV4
+
+export const generateNumberSequence = (startNumber, stopNumber) => {
+    const numberSequenceArray = [];
+    if (stopNumber > startNumber) {
+        for (let anyNumber = startNumber; (anyNumber <= stopNumber); anyNumber += 1) {
+            numberSequenceArray.push(anyNumber);
+        }
+        return numberSequenceArray;
+    }
+    if (startNumber > stopNumber) {
+        for (let anyNumber = startNumber; (anyNumber >= stopNumber); anyNumber -= 1) {
+            numberSequenceArray.push(anyNumber);
+        }
+        return numberSequenceArray;
+    }
+    return numberSequenceArray;
+};
+
+export const isNan = (num) => Number.isNaN(parseFloat(num));
+
+export const stringToHexColor = (anyString) => {
+    let theHash = 0;
+    let anIndex;
+
+    /* eslint-disable no-bitwise */
+    for (anIndex = 0; anIndex < anyString.length; anIndex += 1) {
+        theHash = anyString.charCodeAt(anIndex) + ((theHash << 5) - theHash);
+    }
+
+    let hexColorResult = '#';
+
+    for (anIndex = 0; anIndex < 3; anIndex += 1) {
+        const value = (theHash >> (anIndex * 8)) & 0xff;
+        hexColorResult += `00${value.toString(16)}`.slice(-2);
+    }
+    /* eslint-enable no-bitwise */
+
+    return hexColorResult;
+};
+
+export const removeDuplicateStringFromArrayOfString = (anyArray) => {
+    const seen = {};
+    const uniqueArray = [];
+
+    anyArray.forEach((item) => {
+        if (seen?.[item] === undefined) {
+            seen[item] = true;
+            uniqueArray.push(item);
+        }
+    });
+
+    return uniqueArray;
+};
