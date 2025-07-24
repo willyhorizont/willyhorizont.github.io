ca39835f8ceb371a61a48041655b8807f762ffbe
willyhorizont <willyhorizont@gmail.com>
2025-07-24 07:11:32 +0700

    format code

--- utils-web.js
+++ utils-web.js
@@ -6,3 +6,9 @@ const setHtmlElementPropertyValue = (htmlElement, arrayOfEntryProperty) => {
 const htmlTemplateStringToHtmlElement = (htmlTemplateString) => (((htmlElementTemplate) => ([Object.assign(htmlElementTemplate, { innerHTML: htmlTemplateString }), htmlElementTemplate.content.firstElementChild].at(-1)))(document.createElement("template")));
 const createHtmlElement = (htmlStringTemplate, { arrayOfEntryDataset, arrayOfEntryAttribute, arrayOfElementConfig, arrayOfEntryProperty } = {}) => (((htmlElement) => ([((arrayOfEntryDataset) ? (arrayOfEntryDataset.forEach(([datasetKeyInKebabCase, datasetValue]) => htmlElement.setAttribute(`data-${datasetKeyInKebabCase}`, String(datasetValue)))) : undefined), ((arrayOfElementConfig) ? (arrayOfElementConfig.forEach(({ handlerRefName, eventType, elementHandler, eventHandler }) => (((eventHandlerInner) => ([Object.assign(htmlElement, { [handlerRefName]: eventHandlerInner }), htmlElement.addEventListener(eventType, eventHandlerInner), undefined].at(-1)))(eventHandler || elementHandler(htmlElement))))) : undefined), ((arrayOfEntryAttribute) ? (arrayOfEntryAttribute.forEach(([htmlElementAttribute, htmlElementAttributeValue]) => htmlElement.setAttribute(htmlElementAttribute, String(htmlElementAttributeValue)))) : undefined), ((arrayOfEntryProperty) ? (setHtmlElementPropertyValue(htmlElement, arrayOfEntryProperty)) : undefined), htmlElement].at(-1)))(htmlTemplateStringToHtmlElement(htmlStringTemplate)));
 const appendChildrenReturnParent = (htmlElementParent, ...htmlElementChildren) => ([(htmlElementChildren.forEach((htmlElementChild) => (htmlElementParent.appendChild(htmlElementChild)))), htmlElementParent].at(-1));
+const utilsWeb = {
+    setHtmlElementPropertyValue,
+    htmlTemplateStringToHtmlElement,
+    createHtmlElement,
+    appendChildrenReturnParent,
+};
