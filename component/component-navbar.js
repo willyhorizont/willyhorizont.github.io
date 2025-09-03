21620c8b90222dba6f3535c0e4c08ff071a2e980
willyhorizont <willyhorizont@gmail.com>
2025-09-04 04:57:19 +0700

    fix component-navbar.js

--- component/component-navbar.js
+++ component/component-navbar.js
@@ -10,7 +10,7 @@ req:
     function toggleNavbarMenu() {
         const htmlElementNavbar = document.getElementById("navbar");
         const htmlElementNavbarMenuList = htmlElementNavbar.querySelector("ul");
-        const htmlElementHamburgerMenuButton = htmlElementNavbar.querySelector(".navbar-hamburger-menu-button");
+        const htmlElementHamburgerMenuButton = htmlElementNavbar.querySelector('[id="open-hamburger-menu-button"]');
         const isNavbarMenuSmallScreen = htmlElementNavbar.classList.toggle("navbar-menu-small-screen");
 
         function handleEventClickOutsideHtmlElementNavbarMenuSmallScreen(event) {
@@ -28,10 +28,11 @@ req:
         }
     }
 
-    function toggleTheme(htmlElementToggleThemeButton) {
+    function toggleTheme(event) {
         const newTheme = ((document.documentElement.getAttribute("data-theme") === "dark") ? "light" : "dark");
         document.documentElement.setAttribute("data-theme", newTheme);
-        htmlElementToggleThemeButton.querySelector("button").textContent = ((newTheme === "dark") ? "🌞" : "🌙");
+        const htmlElementToggleThemeButton = event.target;
+        htmlElementToggleThemeButton.textContent = ((newTheme === "dark") ? "🌞" : "🌙");
         localStorage.setItem("theme", newTheme);
     }
 
@@ -43,7 +44,7 @@ req:
 
     const htmlElementHeader = document.body.querySelector("header");
 
-    htmlElementHeader.appendChild(UtilsWeb.createHtmlElement(/*html*/`
+    const htmlElementNavbar = UtilsWeb.createHtmlElement(/*html*/`
         <nav id="navbar" class="navbar">
             <a href="/" class="app-logo">
                 <img src="${relativePath}android-chrome-192x192.png" alt="Willy Horizont's logo" width="48px" height="48px" />
@@ -57,13 +58,19 @@ req:
                 <li data-id="navbar-menu-item" class="navbar-menu-item"><a href="/about">About</a></li>
                 <li data-id="navbar-menu-item" class="navbar-menu-item"><a href="/support">Support</a></li>
                 <li data-id="navbar-menu-item" class="navbar-menu-item"><a href="/contact">Contact</a></li>
-                <li class="navbar-menu-item toggle-theme-button-container" onclick="toggleTheme(this)">
+                <li class="navbar-menu-item toggle-theme-button-container" id="toggle-theme-button-container">
                     <button id="toggle-theme-button" class="toggle-theme-button">🌙</button>
                 </li>
-                <li class="navbar-menu-item navbar-hamburger-menu-close-button" onclick="toggleNavbarMenu()">✖</li>
+                <li class="navbar-menu-item navbar-hamburger-menu-close-button" id="close-hamburger-menu-button">✖</li>
             </ul>
-            <span class="navbar-hamburger-menu-button" onclick="toggleNavbarMenu()">&#9776;</span>
-        </nav>`));
+            <span class="navbar-hamburger-menu-button" id="open-hamburger-menu-button">&#9776;</span>
+        </nav>`);
+    
+    htmlElementNavbar.querySelector('[id="toggle-theme-button-container"]').addEventListener("click", toggleTheme);
+    htmlElementNavbar.querySelector('[id="close-hamburger-menu-button"]').addEventListener("click", toggleNavbarMenu);
+    htmlElementNavbar.querySelector('[id="open-hamburger-menu-button"]').addEventListener("click", toggleNavbarMenu);
+
+    htmlElementHeader.appendChild(htmlElementNavbar);
 
     htmlElementHeader.querySelectorAll('[data-id="navbar-menu-item"]').forEach((htmlElementNavbarMenuItem) => {
         const htmlElementNavbarMenuItemAnchorTagHref = htmlElementNavbarMenuItem.querySelector("a").getAttribute("href");
