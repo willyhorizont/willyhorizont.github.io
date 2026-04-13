9322fdcab8c083cdcd8e988dd35fb03911a3684e
willyhorizont <willyhorizont@gmail.com>
2026-04-13 14:43:30 +0700

    update footer, update remove-duplicate-list-item

--- component/component-footer.js
+++ component/component-footer.js
@@ -4,13 +4,18 @@
     const htmlElementFooter = document.body.querySelector("footer");
 
     htmlElementFooter.appendChild(WillyHorizont.UtilsWeb.htmlTemplateStringToHtmlElement(/*html*/`
-            <div style="display: flex; flex-direction: column; align-items: center; padding: 8px;">
+            <div style="display: flex; flex-direction: column; align-items: center; border-top: 1px solid var(--light-border-color); margin-top: 16px;">
                 <p>Made with ❤️ by Willy Horizont</p>
-                <p id="clock"></p>
+                <div id="footer" style="display: flex; flex-direction: row; justify-content: center; flex-wrap: wrap; width: 100%; background-color: var(--accent-color-2); color: var(--dark-text-color);">
+                </div>
             </div>
         `));
 
     setInterval(() => {
-        document.getElementById("clock").textContent = WillyHorizont.Utils.generateTimestamp({ includeSecond: true, includeMiliSecond: false });
+        const [clockPartAa, clockPartBb, clockPartCc, clockPartDd] = WillyHorizont.Utils.generateTimestamp({ includeSecond: true, includeMiliSecond: false }).split(" | ");
+        document.getElementById("footer").innerHTML = (/*html*/`
+                <p>${clockPartAa} | ${clockPartBb}</p>
+                <p>${clockPartCc} | ${clockPartDd}</p>
+            `);
     }, WillyHorizont.UtilsDate.ONE_SECOND_IN_MILLISECOND);
 })();
