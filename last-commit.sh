51e7b1e56d988d7bc63572012d7f6ed37a94f63c
willyhorizont <willyhorizont@gmail.com>
2026-07-21 15:53:58 +0700

    [Last updated: 21 Jul 2026 @ 03:53 PM] version 2.3.1: update cv layout; fix dark mode;

--- last-commit.sh
+++ last-commit.sh
@@ -2,7 +2,7 @@
 
 SD=$(dirname "$(realpath "$0")")
 RD=$(realpath "$SD")
-V="2.3.0" # ! DON'T FORGET TO CHANGE VERSION BEFORE RUNNING !!!!
+V="2.3.1" # ! DON'T FORGET TO CHANGE VERSION BEFORE RUNNING !!!!
 T=$(date "+%d %b %Y @ %I:%M %p")
 cd "$RD" || exit
 \. "$HOME/.nvm/nvm.sh"
@@ -14,7 +14,8 @@ version $V:
 H=$(sed -e '/./,$!d' <<< "$H")
 # ! DON'T FORGET TO CHANGE COMMIT MESSAGE BEFORE RUNNING !!!!
 M="
-update sw.js, Stale-While-Revalidate, use cache while fetch new data;
+update cv layout;
+fix dark mode;
 "
 M=$(sed -e '/./,$!d' <<< "$M")
 M="$H
